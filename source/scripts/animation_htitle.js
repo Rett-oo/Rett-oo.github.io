d4621284071fc59ff310906a0607a7e71c03a254
Rett-oo <106655967+Rett-oo@users.noreply.github.com>
2022-12-13 20:04:11 +0300

    first upload

---
+++ source/scripts/animation_htitle.js
@@ -0,0 +1,160 @@
+(function() {
+  var $animate, $container, $message, $paragraph, MESSAGES, animate, initialise, scramble;
+
+  MESSAGES = [];
+
+  MESSAGES.push({
+    delay: 0,
+    text: "Greetings!.."
+  });
+
+  MESSAGES.push({
+    delay: 1200,
+    text: "You are on my portfolio site."
+  });
+
+  MESSAGES.push({
+    delay: 2200,
+    text: "Here you can see all the projects I'm working on, as well as those I plan to work on."
+  });
+
+  MESSAGES.push({
+    delay: 4600,
+    text: "You will be able to view the source code of projects with links to the Git-Hub repositories.."
+  });
+
+
+
+  $container = $(".title_container");
+
+  $message = $("#message");
+
+  $animate = $("#animate");
+
+  $paragraph = null;
+
+  scramble = function(element, text, options) {
+    var $element, addGlitch, character, defaults, ghostCharacter, ghostCharacters, ghostLength, ghostText, ghosts, glitchCharacter, glitchCharacters, glitchIndex, glitchLength, glitchProbability, glitchText, glitches, i, j, letter, object, order, output, parameters, ref, settings, shuffle, target, textCharacters, textLength, wrap;
+    // Default properties.
+    defaults = {
+      probability: 0.2,
+      glitches: '-|/\\',
+      blank: '',
+      duration: text.length * 40,
+      ease: 'easeInOutQuad',
+      delay: 0.0
+    };
+    // Convert the element to a jQuery object and build the settings object.
+    $element = $(element);
+    settings = $.extend(defaults, options);
+    // Convenience methods.
+    shuffle = function() {
+      if (Math.random() < 0.5) {
+        return 1;
+      } else {
+        return -1;
+      }
+    };
+    wrap = function(text, classes) {
+      return `<span class="${classes}">${text}</span>`;
+    };
+    // Glitch values.
+    glitchText = settings.glitches;
+    glitchCharacters = glitchText.split('');
+    glitchLength = glitchCharacters.length;
+    glitchProbability = settings.probability;
+    glitches = (function() {
+      var j, len, results;
+      results = [];
+      for (j = 0, len = glitchCharacters.length; j < len; j++) {
+        letter = glitchCharacters[j];
+        results.push(wrap(letter, 'glitch'));
+      }
+      return results;
+    })();
+    // Ghost values.
+    ghostText = $element.text();
+    ghostCharacters = ghostText.split('');
+    ghostLength = ghostCharacters.length;
+    ghosts = (function() {
+      var j, len, results;
+      results = [];
+      for (j = 0, len = ghostCharacters.length; j < len; j++) {
+        letter = ghostCharacters[j];
+        results.push(wrap(letter, 'ghost'));
+      }
+      return results;
+    })();
+    // Text values.
+    textCharacters = text.split('');
+    textLength = textCharacters.length;
+    // Order and output arrays.
+    order = (function() {
+      var results = [];
+      for (var j = 0; 0 <= textLength ? j < textLength : j > textLength; 0 <= textLength ? j++ : j--){ results.push(j); }
+      return results;
+    }).apply(this).sort(this.shuffle);
+    output = [];
+// Build the output array.
+    for (i = j = 0, ref = textLength; (0 <= ref ? j < ref : j > ref); i = 0 <= ref ? ++j : --j) {
+      glitchIndex = Math.floor(Math.random() * (glitchLength - 1));
+      glitchCharacter = glitches[glitchIndex];
+      ghostCharacter = ghosts[i] || settings.blank;
+      addGlitch = Math.random() < glitchProbability;
+      character = addGlitch ? glitchCharacter : ghostCharacter;
+      output.push(character);
+    }
+    // Animate the text.
+    object = {
+      value: 0
+    };
+    target = {
+      value: 1
+    };
+    parameters = {
+      duration: settings.duration,
+      ease: settings.ease,
+      step: function() {
+        var index, k, progress, ref1;
+        progress = Math.floor(object.value * (textLength - 1));
+        for (i = k = 0, ref1 = progress; (0 <= ref1 ? k <= ref1 : k >= ref1); i = 0 <= ref1 ? ++k : --k) {
+          index = order[i];
+          output[index] = textCharacters[index];
+        }
+        return $element.html(output.join(''));
+      },
+      complete: function() {
+        return $element.html(text);
+      }
+    };
+    // Animate the text.
+    return $(object).delay(settings.delay).animate(target, parameters);
+  };
+
+  animate = function() {
+    var data, element, index, j, len, options;
+    for (index = j = 0, len = MESSAGES.length; j < len; index = ++j) {
+      data = MESSAGES[index];
+      element = $paragraph.get(index);
+      element.innerText = '';
+      options = {
+        delay: data.delay
+      };
+      scramble(element, data.text, options);
+    }
+  };
+
+  initialise = function() {
+    var index, j, len, text;
+    $animate.click(animate);
+    for (index = j = 0, len = MESSAGES.length; j < len; index = ++j) {
+      text = MESSAGES[index];
+      $message.append("<p>");
+    }
+    $paragraph = $container.find("p");
+    animate();
+  };
+
+  initialise();
+
+}).call(this);
